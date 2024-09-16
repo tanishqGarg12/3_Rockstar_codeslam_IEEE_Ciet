3ac1ebc42007887b7f170fba7eea0785d05211ce
tanishq <tanishqgarg256@gmail.com>
2024-09-16 10:26:11 +0530

    our first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='bg-black'>
+    <div className='text-xl'>
+      Hello world!
+    </div>
+    <div className='bg-red-500'>
+      ddscs
+    </div>
+    
+    </div>
+  )
+}
