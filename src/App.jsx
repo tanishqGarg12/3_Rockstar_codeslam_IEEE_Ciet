ccf6cd19a33d3d5c35624037b25cca99269e6fff
tanishq <tanishqgarg256@gmail.com>
2024-09-16 15:12:00 +0530

    commitng

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Sec4 from './components/Sec4'
+import Sec1 from './components/Sec1'
+import Section4 from "./components/section4"
+import Group from './components/group'
+import Section5 from './components/section5'
+import Sec7 from './components/Sec7'
+import Footer from './components/Footer'
+
+
 
 export default function App() {
   return (
-    <div className='bg-black'>
-    <div className='text-xl'>
-      Hello world!
-    </div>
-    <div className='bg-red-500'>
-      ddscs
-    </div>
-    
-    </div>
+    <>
+    <Sec1/>
+    <Group/>
+    <Sec4/>
+    <Section4/>
+    <Section5/>
+    <Sec7/>
+    <Footer/>
+    </>
+
   )
 }
